9a862c65a24fb82a44e4d600bd7314814081b523
antonio <antonio@inspiron-7501.casa>
2025-08-05 14:31:44 +0200

    Riorganizzazione file

--- Codici/F10_2_4.cpp
+++ Codici/F10_2_4.cpp
@@ -11,7 +11,7 @@
 
 #include <cmath>
 #include <cstdio>
-#define TOL 1.e-13
+#define TOL 1.e-5
 #define MAXIT 100
 
 
@@ -20,13 +20,13 @@ double funzione(int, double);
 
 double derivata(int, double);
 
-int newton(double&, double, double, int);
+int newton(double&, double, int);
 
 int main(void){
-    double a, b, x, sol, f; int iter, s;
+    double a, b, sol, f; int iter, s;
     do{
     printf("Selezionare una funzione\n");
-    printf("1 = x^3+2y^3 = 3xy\n");
+    printf("1 = x^2 - 2\n");
     printf("2 = x^5 - 6x^3 + 3\n");
     printf("3 = x - e^(-x)\n");
     printf("4 = x^2 - sin(x)\n");
@@ -40,17 +40,15 @@ int main(void){
     scanf("%lf", &b);
     printf("\nIndicare l'estremo di Fourier (1 => a, 2 => b): ");
     scanf("%lf", &f); f-=1; f = int(f);
-    printf("\nInserisci il valore di x = ");
-    scanf("%lf", &x);
     if(b < a || f < 0 || f > 1 || s < 1 || s > 6)
         printf("\nDati non validi. Reinserirli\n");}
     while( b < a || f < 0 || f > 1 || s < 1 || s > 6);
 
     //al posto di acquisirlo nuovamente basta selezionarlo tra i due inseriti prima
     f = (f)? b : a;
-    iter = newton(f, x, TOL, s);
+    iter = newton(f, TOL, s);
 
-    printf("Soluzione approssimata dal metodo di Newton con precisione %e --> %.14lf\nIterazioni del metodo eseguite --> %d\n",TOL, f, iter);
+    printf("Soluzione approssimata dal metodo di Newton con precisione 10^-5 --> %.13lf\nIterazioni del metodo eseguite --> %d\n", f, iter);
     return 0;
 
 }
@@ -58,10 +56,10 @@ int main(void){
 
 
 
-double funzione(int s, double y, double x){
+double funzione(int s, double x){
     
     switch(s){
-        case 1: return (x*x*x+5*y*y*y-3*x*y); break;
+        case 1: return (x*x-2); break;
         case 2: return (x*x*x*(x*x - 6))+3; break;
         case 3: return x - (1/exp(x)); break;
         case 4: return x*x - sin(x); break;
@@ -71,10 +69,10 @@ double funzione(int s, double y, double x){
     return 1;
 }
 
-double derivata(int s, double y, double x){
+double derivata(int s, double x){
 
     switch(s){
-        case 1: return 15*y*y-3*x; break;
+        case 1: return 2*x; break;
         case 2: return (x*x*(5*x*x - 18)); break;
         case 3: return 1 + (1/exp(x)); break;
         case 4: return 2*x - cos(x); break; 
@@ -84,11 +82,11 @@ double derivata(int s, double y, double x){
     return 1;       
 }
 
-int newton(double& y, double x,  double tau, int s){
+int newton(double& x, double tau, int s){
     int cont = 0; double delta;
     do{
-        delta = funzione(s, y, x)/derivata(s, y, x);        //questo delta equivale a x_i - x_(i-1), che ci consente di utilizzare il criterio d'arresto praticamente con 1 solo controllo (e della memoria occupata) e nessun operazione in più 
-        y -= delta;
+        delta = funzione(s, x)/derivata(s, x);        //questo delta equivale a x_i - x_(i-1), che ci consente di utilizzare il criterio d'arresto praticamente con 1 solo controllo (e della memoria occupata) e nessun operazione in più 
+        x -= delta;
     }while(++cont < MAXIT &&(delta >= tau || delta <= -tau));
     return cont;
 }
